75e54b565dc41a22c74a8a01a5e7d72173027a81
karim khabouri <karimkhabouri@gmail.com>
2021-02-07 14:17:45 +0100

    docker compose

---
+++ server/spark/src/main/java/Server.java
@@ -0,0 +1,46 @@
+import static spark.Spark.*;
+
+public class Server {
+
+
+    public static void main(String[] arg){
+
+        port(80);
+
+
+
+        get("/hello", (request, response) -> "Hello World!");
+
+        post("/hello", (request, response) -> "Hello World: " + request.body());
+        
+
+        
+
+        get("/private", (request, response) -> {
+            response.status(401);
+            return "Go Away!!!";
+        });
+
+        
+        get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+
+
+        get("/news/:section", (request, response) -> {
+            response.type("text/xml");
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>";
+        });
+
+
+        get("/protected", (request, response) -> {
+            halt(403, "I don't think so!!!");
+            return null;
+        });
+
+
+        get("/redirect", (request, response) -> {
+            response.redirect("/news/world");
+            return null;
+        });
+
+    }
+}
